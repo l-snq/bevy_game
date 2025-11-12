aa366ae8af973eb9ee93afaeb6fe3574811856c1
snackkk <tastytothesupreme@gmail.com>
2025-11-11 23:23:36 -0700

    added Player as a query parameter

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ fn setup_player(mut commands: Commands, asset_server: Res<AssetServer>) {
 const MENU_BOX: Color = Color::srgb(2.0, 3.0, 4.0);
 
 fn setup_prompt(mut commands: Commands) {
-    let player_id = commands.spawn((
+    commands.spawn((
         Sprite::from_color(MENU_BOX, Vec2::ONE),
         Transform::default()
         .with_translation(Vec3::new(1.0, 1.0, 1.0))
@@ -40,7 +40,7 @@ fn setup_prompt(mut commands: Commands) {
 }
 
 // figure out how to query by making Struct Markers (such as struct PLayer)
-fn query_entities(mut commands: Commands, mut query: Query<(Entity, &mut Transform)>) {
+fn query_entities(mut commands: Commands, mut query: Query<(Entity, &mut Transform, )>) {
     for (e, transform) in query.iter() {
         if let Ok(entity) = commands.get_entity(e) {
             println!("entity IDs: {:?}", entity.id());
@@ -49,8 +49,8 @@ fn query_entities(mut commands: Commands, mut query: Query<(Entity, &mut Transfo
 }
 
 fn player_physics(keys: Res<ButtonInput<KeyCode>>, 
-    mut query: Query<(&Sprite, &mut Transform)>) {
-    for (sprite, mut transform) in &mut query {
+    mut query: Query<(&Sprite, &mut Transform, Player)>) {
+    for (sprite, mut transform, Player) in &mut query {
             if keys.pressed(KeyCode::KeyW) {
                 transform.translation.y += 1.0;
             } else if keys.pressed(KeyCode::KeyD) {
