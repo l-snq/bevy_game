2b054b4195e78b2e9a26dd1d24429a5b9a00918b
snackkk <tastytothesupreme@gmail.com>
2025-11-11 22:20:22 -0700

    updated player transparency

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup_player)
-        .add_systems(Update, query_entities)
         .add_systems(Update, setup_prompt)
         .add_systems(Update, player_physics)
         .run();
