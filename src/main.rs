295b3541e1ec3c5b607daf96c9847379f76cf022
snackkk <tastytothesupreme@gmail.com>
2025-11-11 21:58:24 -0700

    fuck i am trying to just access the fucking entity id holy shit

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use bevy::{picking::input::spawn_mouse_pointer, prelude::*};
+use bevy::{ecs::world::EntityFetcher, prelude::*};
 
 #[derive(Default, Component)]
-struct Player {
-    coordinates: f32,
+struct Player;
+
+#[derive(Default, Component)]
+struct GameState {
+    prompt: bool,
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup_player)
+        .add_systems(Update, query_entities)
+        .add_systems(Update, setup_prompt)
         .add_systems(Update, player_physics)
         .run();
 }
@@ -23,16 +28,35 @@ fn setup_player(mut commands: Commands, asset_server: Res<AssetServer>) {
     ));
 }
 
-fn player_physics(keys: Res<ButtonInput<KeyCode>>, mut query: Query<(&Sprite, &mut Transform)>) {
-    for (sprite, mut transform) in &mut query {
-        if keys.pressed(KeyCode::KeyW) {
-            transform.translation.y += 1.0;
-        } else if keys.pressed(KeyCode::KeyD) {
-            transform.translation.x += 1.0;
-        } else if keys.pressed(KeyCode::KeyA) {
-            transform.translation.x -= 1.0;
-        } else if keys.pressed(KeyCode::KeyS) {
-            transform.translation.y -= 1.0;
+const MENU_BOX: Color = Color::srgb(2.0, 3.0, 4.0);
+
+fn setup_prompt(mut commands: Commands) {
+    commands.spawn((
+        Sprite::from_color(MENU_BOX, Vec2::ONE),
+        Transform::from_translation(Vec3::new(1.0, 1.0, 1.0)),
+        Player {},
+    ));
+}
+
+fn query_entities(mut commands: Commands, mut query: Query<(Entity, &mut Transform)>) {
+    for (e, transform) in query.iter() {
+        if let Ok(entity) = commands.get_entity(e) {
+            println!("entity IDs: {:?}", entity.id());
         }
+    }
+}
+
+fn player_physics(keys: Res<ButtonInput<KeyCode>>, 
+    mut query: Query<(&Sprite, &mut Transform)>) {
+    for (sprite, mut transform) in &mut query {
+            if keys.pressed(KeyCode::KeyW) {
+                transform.translation.y += 1.0;
+            } else if keys.pressed(KeyCode::KeyD) {
+                transform.translation.x += 1.0;
+            } else if keys.pressed(KeyCode::KeyA) {
+                transform.translation.x -= 1.0;
+            } else if keys.pressed(KeyCode::KeyS) {
+                transform.translation.y -= 1.0;
+            }
     }
 }
